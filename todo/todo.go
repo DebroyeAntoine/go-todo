d1fcddc86542765e2a2e8f4461919ac10c80d56f
Antoine Debroye <ant.debroye@gmail.com>
2025-03-03 18:03:24 +0100

    tests: Add some other tests on Add method

--- todo/todo.go
+++ todo/todo.go
@@ -1,6 +1,11 @@
 package todo
 
+import (
+	"reflect"
+)
+
 type TaskStatus int
+type TodoErr string
 
 const (
 	NotStarted TaskStatus = iota
@@ -8,14 +13,56 @@ const (
 	Done
 )
 
+const (
+	AlreadyPresent = TodoErr("could not add this task because it already exists")
+	IdConflict     = TodoErr("the task id is already stored in the todo")
+)
+
+func (e TodoErr) Error() string {
+	return string(e)
+}
+
 type Task struct {
-	Id     int
-	Task   string
-	Status TaskStatus
+	Id        int
+	TaskTitle string
+	Status    TaskStatus
 }
 
 type Todo []Task
 
-func (t *Todo) Add(task Task) {
+func (t *Todo) isExisting(task Task) bool {
+	for _, todoitem := range *t {
+		if todoitem.Id == task.Id && todoitem.TaskTitle == task.TaskTitle &&
+			todoitem.Status == task.Status {
+			return true
+		}
+	}
+	return false
+}
+
+func (t *Todo) isConflictingId(task Task) bool {
+	for _, todoitem := range *t {
+		if todoitem.Id == task.Id {
+			return true
+		}
+	}
+	return false
+}
+
+func (t *Todo) Add(task Task) error {
+	if t.isExisting(task) {
+		return AlreadyPresent
+	}
+	if t.isConflictingId(task) {
+		return IdConflict
+	}
+	if task.Id == 0 {
+		if len(*t) == 0 {
+			task.Id = 1
+		} else {
+			task.Id = (*t)[len(*t)-1].Id + 1
+		}
+	}
 	*t = append(*t, task)
+	return nil
 }
--- todo/todo_test.go
+++ todo/todo_test.go
@@ -5,17 +5,74 @@ import (
 	"testing"
 )
 
+func AssertErrors(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
 func TestAddTask(t *testing.T) {
 
-	task1 := Task{0, "test", InProgress}
-	tmp := &Todo{}
-	tmp.Add(task1)
-	got := *tmp
-	want := Todo{{0, "test", InProgress}}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Logf("got: %#v, want: %#v", got, want)
-
-		t.Errorf("got %v, want %v", got, want)
+	checkAdd := func(t testing.TB, got, want Todo) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("got %v, want %v", got, want)
+		}
 	}
+
+	t.Run("Add a new full struct task", func(t *testing.T) {
+		task1 := Task{1, "test", InProgress}
+		list := &Todo{}
+		err := list.Add(task1)
+		got := *list
+		want := Todo{{1, "test", InProgress}}
+
+		checkAdd(t, got, want)
+		AssertErrors(t, err, nil)
+	})
+
+	t.Run("Add a task without id", func(t *testing.T) {
+		task2 := Task{TaskTitle: "test2", Status: NotStarted}
+		list := &Todo{{9, "another task", Done}}
+		err := list.Add(task2)
+		got := *list
+		want := Todo{{9, "another task", Done}, {10, "test2", NotStarted}}
+
+		checkAdd(t, got, want)
+		AssertErrors(t, err, nil)
+	})
+
+	t.Run("Add a task without completion", func(t *testing.T) {
+		taskNotStarted := Task{Id: 12, TaskTitle: "not Started"}
+		list := &Todo{}
+		err := list.Add(taskNotStarted)
+		got := *list
+		want := Todo{{12, "not Started", NotStarted}}
+
+		checkAdd(t, got, want)
+		AssertErrors(t, err, nil)
+	})
+
+	t.Run("Add an existing task", func(t *testing.T) {
+		task := Task{Id: 12, TaskTitle: "already exist"}
+		list := &Todo{task}
+		err := list.Add(task)
+		got := *list
+		want := Todo{{12, "already exist", NotStarted}}
+
+		checkAdd(t, got, want)
+		AssertErrors(t, err, AlreadyPresent)
+	})
+
+	t.Run("Add task with an existing id", func(t *testing.T) {
+		task := Task{Id: 99, TaskTitle: "id already exist"}
+		list := &Todo{{Id: 99, TaskTitle: "another task"}}
+		err := list.Add(task)
+		got := *list
+		want := Todo{{99, "another task", NotStarted}}
+
+		checkAdd(t, got, want)
+		AssertErrors(t, err, IdConflict)
+	})
 }
